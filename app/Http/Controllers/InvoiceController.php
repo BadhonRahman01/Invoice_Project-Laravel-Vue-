a312b9582ab777a270e6089a7c856ec002516e99
BadhonRahman01 <badhon.rahman184@gmail.com>
2023-07-27 17:23:03 +0600

    InvoicesLoaded

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Invoice;
 
 class InvoiceController extends Controller
 {
-    //
+    public function get_all_invoices(){
+        $invoices = Invoice::with('customer')->orderBy('id', 'desc')->get();
+        return response()->json([
+            'invoices' => $invoices]
+        , 200);
+    }
 }
